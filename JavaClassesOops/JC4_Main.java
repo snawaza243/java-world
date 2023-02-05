38c8a654ec83818a082af95a43f9f0fde5dade50
Shahnawaz Alam <91892524+snawaza243@users.noreply.github.com>
2023-02-05 19:42:15 +0530

    update

---
+++ JavaClassesOops/JC4_Main.java
@@ -0,0 +1,9 @@
+public class JC4_Main {
+    public void publicMeth1() {
+        System.out.println("Public method with multiple classes 1");
+    }
+
+    public void publicMeth2(int n2) {
+        System.out.println("Public method with multiple classes 2 num = " + n2);
+    }
+}
---
+++ JavaClassesOops/JC4_Second.java
@@ -0,0 +1,8 @@
+public class JC4_Second {
+    public static void main(String[] args) {
+        JC4_Main multiClassMeth = new JC4_Main();
+
+        multiClassMeth.publicMeth1();
+        multiClassMeth.publicMeth2(2023);
+    }
+}
