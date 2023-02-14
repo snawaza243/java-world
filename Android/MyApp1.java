e70ef67187f1bbd96445aa083c6bb58a6fb4f1d1
Shahnawaz Alam <91892524+snawaza243@users.noreply.github.com>
2023-02-14 12:20:02 +0530

    update

---
+++ Android/MyApp1.java
@@ -0,0 +1,50 @@
+// package com.example.myfirstapp;
+
+// import androidx.appcompat.app.AppCompatActivity;
+// import android.os.Bundle;
+// import android.util.Log;
+// import android.widget.Toast;
+
+// public class MyApp1 extends AppCompatActivity {
+
+//     @Override
+//     protected void onCreate(Bundle savedInstanceState) {
+//         super.onCreate(savedInstanceState);
+//         setContentView(R.layout.activity_main);
+//         Log.i("myFirstApp","I am onCreate ");
+//         Toast.makeText(getApplicationContext(), "I am onCreate",Toast.LENGTH_SHORT).show();
+//     }
+
+//     @Override
+//     protected void onStart() {
+//         super.onStart();
+//         Log.i("myFirstApp","I am onStart");
+//         Toast.makeText(getApplicationContext(), "I am onStart",Toast.LENGTH_SHORT).show();
+//     }
+//     @Override
+//     protected  void onResume(){
+//         super.onResume();
+//         Log.i("myFirstApp", "I am onResume");
+//         Toast.makeText(getApplicationContext(), "I am onResume",Toast.LENGTH_SHORT).show();
+//     }
+//     @Override
+//     protected  void onPause(){
+//         super.onPause();
+//         Log.i("myFirstApp", "I am onPause");
+//         Toast.makeText(getApplicationContext(), "I am onPause",Toast.LENGTH_SHORT).show();
+//     }
+//     @Override
+//     protected  void onStop(){
+//         super.onStop();
+//         Log.i("myFirstApp", " I am onStop");
+//         Toast.makeText(getApplicationContext(), "I am onStop",Toast.LENGTH_SHORT).show();
+//     }
+
+//     @Override
+//     protected  void onDestroy(){
+//         super.onDestroy();
+//         Log.i("myFirstApp", " I am onDestroy");
+//         Toast.makeText(getApplicationContext(), "I am onDestroy",Toast.LENGTH_SHORT).show();
+//     }
+
+// }
---
+++ Android/MyApp2.java
@@ -0,0 +1,60 @@
+// package com.example.mysecondapp;
+
+// import androidx.appcompat.app.AppCompatActivity;
+
+// import android.os.Bundle;
+// import android.view.View;
+// import android.widget.Button;
+// import android.widget.EditText;
+// import android.widget.Toast;
+// import android.widget.TextView;
+// public class MainActivity extends AppCompatActivity {
+//     Button b1,b2,b3,b4;
+//     EditText etv1,etv2;
+    
+//     @Override
+//     protected void onCreate(Bundle savedInstanceState) {
+//         super.onCreate(savedInstanceState);
+//         setContentView(R.layout.activity_main);
+
+//         b1 = findViewById(R.id.b1);
+//         b2 = findViewById(R.id.b2);
+//         b3 = findViewById(R.id.b3);
+//         b4 = findViewById(R.id.b4);
+
+//         etv1 = findViewById(R.id.et1);
+//         etv2 = findViewById(R.id.et2);
+//     }
+
+//     public void btnAdd(View view) {
+//         String a= etv1.getText().toString();
+//         String b = etv2.getText().toString();
+
+//         int c= Integer.parseInt(a)+ Integer.parseInt(b);
+//         Toast.makeText(getApplicationContext(), "Addition is " + c, Toast.LENGTH_SHORT).show();
+//     }
+
+//     public void btnSub(View view) {
+//         String a= etv1.getText().toString();
+//         String b = etv2.getText().toString();
+
+//         int c= Integer.parseInt(a)- Integer.parseInt(b);
+//         Toast.makeText(getApplicationContext(), "Subtraction (a-b) is " + c, Toast.LENGTH_SHORT).show();
+//     }
+
+//     public void btnMul(View view) {
+//         String a= etv1.getText().toString();
+//         String b = etv2.getText().toString();
+
+//         int c= Integer.parseInt(a)* Integer.parseInt(b);
+//         Toast.makeText(getApplicationContext(), "Multiplication is " + c, Toast.LENGTH_SHORT).show();
+//     }
+
+//     public void btnDiv(View view) {
+//         String a= etv1.getText().toString();
+//         String b = etv2.getText().toString();
+
+//         int c= Integer.parseInt(a)/ Integer.parseInt(b);
+//         Toast.makeText(getApplicationContext(), "Division (a/b) is " + c, Toast.LENGTH_SHORT).show();
+//     }
+// }
