7ed1b0ec240154b3dce445d9d72ad087eb05fcf0
Shahnawaz Alam <91892524+snawaza243@users.noreply.github.com>
2024-03-14 23:02:22 +0530

    Divide and conquer v6

--- jd/20-Divide-Concure/DCA_Merge.java
+++ jd/20-Divide-Concure/DCA_Merge.java
@@ -7,27 +7,29 @@
  * 
  * a. divide prob into left and right part and retrieve mid index
  * b. merge sort left, right
- * c. merge left+right in temp array
+ * c. merge left+right in temp array and temp to org
  * 
  */
 public class DCA_Merge {
 
     public static void printArr(int arr[]) {
         for (int i = 0; i < arr.length; i++) {
-            System.out.println(arr[i]);
+            System.out.print(arr[i] + " ");
         }
     }
 
     public static void mergeSort(int arr[], int si, int ei) {
 
+        // base case
         if (si >= ei) {
             return;
         }
+
         // kaam
         int mid = si + (ei - si) / 2;
-        mergeSort(arr, si, mid);
-        mergeSort(arr, mid + 1, ei);
-        merge(arr, si, mid, ei);
+        mergeSort(arr, si, mid); // for left side
+        mergeSort(arr, mid + 1, ei); // for right side
+        merge(arr, si, mid, ei); // merge left right
     }
 
     public static void merge(int arr[], int si, int mid, int ei) {
@@ -36,20 +38,40 @@ public class DCA_Merge {
         int j = mid + 1; // right iterator
         int k = 0; // temp array iterator
 
+        // compare and copy elements
         while (i <= mid && j <= ei) {
             if (arr[i] < arr[j]) {
                 temp[k] = arr[i];
                 i++;
-                k++;
             } else {
                 temp[k] = arr[j];
                 j++;
-                k++;
             }
+            k++;
+        }
+
+        // leftover  in the left side
+        while (i <= mid) {
+            temp[k++] = arr[i++];
+
+        }
+
+        // leftover  in right part is sorted already.
+        while (j <= ei) {
+            temp[k++] = arr[j++];
+        }
+
+
+        // copy temp to org arr
+        for (k = 0, i = si; k < temp.length; k++, i++) {
+            arr[i] = temp[k];
         }
     }
 
     public static void main(String[] args) {
 
+        int arr[] = { 3,7,4,2,6,4,62,8 };
+        mergeSort(arr, 0, arr.length - 1);
+        printArr(arr);
     }
 }
